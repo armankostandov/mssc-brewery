89a1d224510a4d80fc8a92dddd86a80358fb2387
https://akostandov@bitbucket.org/akostandov/edup.git <armankostandov@gmail.com>
2021-03-12 19:49:11 +0300

    add CustomerController REST methods

--- src/main/java/ru/foreverjun/msscbrewery/web/services/CustomerService.java
+++ src/main/java/ru/foreverjun/msscbrewery/web/services/CustomerService.java
@@ -7,4 +7,10 @@ import java.util.UUID;
 
 public interface CustomerService {
     CustomerDto getCustomerById(UUID beerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
 }
--- src/main/java/ru/foreverjun/msscbrewery/web/services/CustomerServiceImpl.java
+++ src/main/java/ru/foreverjun/msscbrewery/web/services/CustomerServiceImpl.java
@@ -1,17 +1,39 @@
 package ru.foreverjun.msscbrewery.web.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.foreverjun.msscbrewery.web.model.BeerDto;
 import ru.foreverjun.msscbrewery.web.model.CustomerDto;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
-public class CustomerServiceImpl implements CustomerService {
+public
+class CustomerServiceImpl implements CustomerService {
     @Override
     public CustomerDto getCustomerById(UUID customerId) {
-        return CustomerDto.builder().id(UUID.randomUUID())
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
                 .name("Joe Buck")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //todo impl
+        log.debug("Updating....");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        log.debug("Deleting.... ");
     }
 }
