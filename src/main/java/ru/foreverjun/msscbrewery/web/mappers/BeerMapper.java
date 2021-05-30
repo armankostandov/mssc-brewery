a3b52baece3e06bbdd603e3c94b880b96e2af381
https://akostandov@bitbucket.org/akostandov/edup.git <armankostandov@gmail.com>
2021-05-30 15:41:53 +0300

    add createdDate and lastUpdatedDate fields to Beer

--- src/main/java/ru/foreverjun/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/ru/foreverjun/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import ru.foreverjun.msscbrewery.domain.Beer;
 import ru.foreverjun.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
